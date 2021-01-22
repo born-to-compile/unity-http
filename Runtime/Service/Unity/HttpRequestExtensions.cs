8a6559e08a3a872a47c27832a21c37567c41299d
Harry <borntocompile@gmail.com>
2021-01-22 18:44:34 +0000

    Http async

---
+++ Runtime/Service/Unity/HttpRequestExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using BornToCompile.AsyncUtilities;
+
+namespace BornToCompile.Http.Service.Unity
+{
+	public static class HttpRequestExtensions
+	{
+		public static async Task<HttpResponse> SendAsync(this IHttpRequest request)
+		{
+			var waitForHttpResponse = new WaitForHttpRequest();
+			HttpResponse response = null;
+
+			void HandleResponse(HttpResponse httpResponse)
+			{
+				response = httpResponse;
+				waitForHttpResponse.HasResponded = true;
+			}
+
+			request
+				.OnSuccess(HandleResponse)
+				.OnError(HandleResponse)
+				.OnNetworkError(HandleResponse)
+				.Send();
+
+			await waitForHttpResponse;
+			return response;
+		}
+	}
+}
---
+++ Runtime/Service/Unity/WaitForHttpRequest.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace BornToCompile.Http.Service.Unity
+{
+	public sealed class WaitForHttpRequest : CustomYieldInstruction
+	{
+		public override bool keepWaiting => !HasResponded;
+		public bool HasResponded { get; set; }
+	}
+}
